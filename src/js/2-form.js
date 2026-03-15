c283182f2bfabdba697e39f14245c64bdc0d6676
Roman Pasichniy <roman.pasichniy@gmail.com>
2026-03-15 13:56:11 +0200

    fix 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,6 +2,13 @@ const form = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
 let formData = { email: '', message: '' };
 
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  formData = JSON.parse(savedData);
+  form.email.value = formData.email || '';
+  form.message.value = formData.message || '';
+}
+
 form.addEventListener('input', event => {
   formData[event.target.name] = event.target.value.trim();
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
